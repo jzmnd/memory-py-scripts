1901383a083d3a626a0b3da93f43e69d3ee235e8
Jeremy Smith <j-smith@eecs.berkeley.edu>
2015-09-09 15:09:01 -0700

    Added argument class and wite only test

---
+++ Arguments.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+"""
+Arguments.py
+Class for parsing memory testing arguments
+Use with memory_test_v2.ino
+
+Created by Jeremy Smith on 2015-07-10
+University of California, Berkeley
+j-smith@ecs.berkeley.edu
+"""
+
+import os
+import sys
+from argparse import ArgumentParser
+
+
+class Arguments(ArgumentParser):
+	"""Class for parsing variables"""
+
+	def __init__(self):
+		super(Arguments, self).__init__()
+
+		self.add_argument('-w', '--wordline', default=0, type=int, choices=xrange(0, 3), help="Word line read number")
+
+		self.add_argument('-a', '--arraysize', default=2, type=int, help="Array size")
+		self.add_argument('-t', '--pattern', default='1100', type=str, help="Pattern to write into array")
+
+		self.add_argument('-r', '--writepulse', default=100, type=int, help="Write pulse time in ms")
+		self.add_argument('-p', '--prechargepulse', default=200, type=int, help="Precharge time in ms")
+		self.add_argument('-l', '--loop', default=1, type=int, help="Number of read/write pulses")
+
+		self.add_argument('filename', nargs='?', type=str, default='output', help="Output file name")
+
+	def parse(self):
+		a = vars(self.parse_args())
+		if len(a['pattern']) != a['arraysize']**2:
+			self.error("Array size and write pattern do not match")
+		if a['arraysize'] == 1:
+			a['filename'] += '_1x1_w{:s}-a{:b}.txt'
+		elif a['arraysize'] == 2:
+			a['filename'] += '_2x2_w{:s}-a{:02b}.txt'
+		else:
+			a['filename'] += '_3x3_w{:s}-a{:03b}.txt'
+		return a
---
+++ memory_test_writeonly.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python
+"""
+memory_test_writeonly.py
+Memory testing python script (write states only with no read)
+Use with memory_test_v2.ino
+
+Created by Jeremy Smith on 2015-07-10
+University of California, Berkeley
+j-smith@ecs.berkeley.edu
+"""
+
+import os
+import sys
+import time
+import numpy as np
+from Arguments import *
+from MemTest import *
+
+__author__ = "Jeremy Smith"
+__version__ = "1.0"
+
+# Define constants
+serialport = '/dev/cu.usbmodem1421'
+
+# Path name for location of script
+path = os.path.dirname(os.path.abspath(__file__))
+
+
+def main():
+	"""Main function"""
+
+	print "\n================================"
+	print "Memory test program (write only)"
+	print __author__
+	print "Version {:s}".format(__version__)
+	print "Email: j-smith@eecs.berkeley.edu"
+	print "================================\n"
+
+	args = Arguments().parse()
+	writelist = []                                       # List of write objects
+
+	for i, c in enumerate(args['pattern']):                    # Creates list of write objects for each CRS device
+		if c == '0':
+			writelist.append(MemTest(serialport, 'writezero', wordline=i//args['arraysize'], bitline=i%args['arraysize'], rtime=args['writepulse'], loop=args['loop']))
+		elif c == '1':
+			writelist.append(MemTest(serialport, 'writeone', wordline=i//args['arraysize'], bitline=i%args['arraysize'], rtime=args['writepulse'], loop=args['loop']))
+		else:
+			print "Write pattern error - use 0 or 1"
+
+	raw_input("\nSet WRITE voltage. Press Enter to continue...")
+	for write in writelist:
+		write.run()
+
+	print "\n===================="
+	print "MEMORY TEST COMPLETE"
+	print "====================\n"
+	return
+
+
+if __name__ == "__main__":
+	sys.exit(main())
